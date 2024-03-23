6f72116387c1ea39ef62ebca546595cfac386124
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-03-22 17:17:56 -0700

    inital commit

---
+++ src/main/java/ca/bestbuy/zephyrtestautomation/ZephyrTestAutomationApplication.java
@@ -0,0 +1,13 @@
+package ca.bestbuy.zephyrtestautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZephyrTestAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZephyrTestAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/bestbuy/zephyrtestautomation/ZephyrTestAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bestbuy.zephyrtestautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZephyrTestAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
